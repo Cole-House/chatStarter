a569b970e157c76d20c3e2903b5a90d2eee14f1e
Cole House <30875198+Cole-House@users.noreply.github.com>
2024-11-02 13:02:10 -1000

    Started enhancing UI with sidebar

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,6 +1,12 @@
 "use client";
 
 import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import {
   Sidebar,
   SidebarContent,
   SidebarGroup,
@@ -11,10 +17,14 @@ import {
   SidebarMenuItem,
   SidebarProvider,
   SidebarGroupAction,
+  SidebarFooter,
 } from "@/components/ui/sidebar";
-import { RedirectToSignIn } from "@clerk/nextjs";
-import { Authenticated, Unauthenticated } from "convex/react";
-import { Link, PlusIcon, User2Icon } from "lucide-react";
+import { api } from "@/convex/_generated/api";
+import { RedirectToSignIn, SignOutButton } from "@clerk/nextjs";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Authenticated, Unauthenticated, useQuery } from "convex/react";
+import { PlusIcon, User2Icon } from "lucide-react";
+import Link from "next/link";
 import React from "react";
 
 export default function DashboardLayout({
@@ -39,6 +49,11 @@ export default function DashboardLayout({
   );
 }
 function DashboardSidebar() {
+  // call user data from the api to the fron-end
+  const user = useQuery(api.functions.user.get);
+  if (!user) {
+    return null;
+  }
   return (
     <Sidebar>
       <SidebarContent>
@@ -55,15 +70,43 @@ function DashboardSidebar() {
               </SidebarMenuItem>
             </SidebarMenu>
           </SidebarGroupContent>
+          <SidebarGroup>
+            <SidebarGroupLabel>Direct Messages</SidebarGroupLabel>
+            <SidebarGroupAction>
+              <PlusIcon />
+              <span className="sr-only"> New Direct Message</span>
+            </SidebarGroupAction>
+          </SidebarGroup>
         </SidebarGroup>
-      </SidebarContent>{" "}
-      <SidebarGroup>
-        <SidebarGroupLabel>Direct Messages</SidebarGroupLabel>
-        <SidebarGroupAction>
-          <PlusIcon />
-          <span className="sr-only"> New Direct Message</span>
-        </SidebarGroupAction>
-      </SidebarGroup>
+      </SidebarContent>
+      <SidebarFooter>
+        <SidebarGroup>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              <SidebarMenuItem>
+                <DropdownMenu>
+                  {/* asChild means the SideBarmenuButton will serve as the dropdown menu trigger */}
+                  <DropdownMenuTrigger asChild>
+                    <SidebarMenuButton className="flex-items-center">
+                      <Avatar className="size-6">
+                        <AvatarImage src={user.image} />
+                        <AvatarFallback> {user.username[0]}</AvatarFallback>
+                      </Avatar>
+                      <p className="font-medium"> {user.username}</p>
+                    </SidebarMenuButton>
+                  </DropdownMenuTrigger>
+                  <DropdownMenuContent>
+                    {/* asChild allows the signout button to work */}
+                    <DropdownMenuItem asChild>
+                      <SignOutButton />
+                    </DropdownMenuItem>
+                  </DropdownMenuContent>
+                </DropdownMenu>
+              </SidebarMenuItem>
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarFooter>
     </Sidebar>
   );
 }
--- convex/functions/user.ts
+++ convex/functions/user.ts
@@ -1,5 +1,11 @@
-import { internalMutation } from "../_generated/server";
+import { internalMutation, MutationCtx, query, QueryCtx } from "../_generated/server";
 import { v } from "convex/values";
+
+export const get = query({
+    handler : async (ctx) => {
+        return await getCurrentUser(ctx);
+    }
+})
 // internal mutation to upsert a user in the database when a user is created or updated in Clerk
 export const upsert = internalMutation({
   args: {
@@ -9,10 +15,7 @@ export const upsert = internalMutation({
   },
   // hnadler is first going to look up the user by their clerkId using the withIndex helper
   handler: async (ctx, args) => {
-    const user = await ctx.db
-      .query("users")
-      .withIndex("byClerkId", (q) => q.eq("clerkId", args.clerkId))
-      .unique();
+    const user = await getUserByClerkId(ctx, args.clerkId);
     // if the user exists, patch the user with the new username and image and if the user does not exist, insert a new user with the username, image, and clerkId
     if (user) {
       await ctx.db.patch(user._id, {
@@ -30,18 +33,33 @@ export const upsert = internalMutation({
 });
 
 export const remove = internalMutation({
-    args: {clerkId: v.string()},
-    handler: async (ctx, { clerkId }) => { 
-        const user = await ctx.db
-        .query("users")
-        .withIndex("byClerkId", (q) => q.eq("clerkId", clerkId))
-        .unique();
-
-        if (user) {
-            await ctx.db.delete(user._id);
-        } else {
-            throw new Error("User not found");
-        }
+  args: { clerkId: v.string() },
+  handler: async (ctx, { clerkId }) => {
+    const user = await getUserByClerkId(ctx, clerkId);
+    if (user) {
+      await ctx.db.delete(user._id);
+    } else {
+      throw new Error("User not found");
     }
+  },
 });
 
+const getCurrentUser = async (ctx: QueryCtx | MutationCtx) => {
+  // we are extracting the clerkID from the identity object
+  const identity = await ctx.auth.getUserIdentity();
+  if (!identity) {
+    return null;
+  }
+  // we are passing in the identity web token userID
+  return await getUserByClerkId(ctx, identity.subject);
+};
+
+const getUserByClerkId = async (
+  ctx: QueryCtx | MutationCtx,
+  clerkId: string
+) => {
+  return await ctx.db
+    .query("users")
+    .withIndex("byClerkId", (q) => q.eq("clerkId", clerkId))
+    .unique();
+};
